4818a6f693ede139b0a06ef3d9e207b8e982738e
jakealmeda <jakealmeda@gmail.com>
2022-02-01 22:28:49 +0800

    1.4.2 | Corrected IF statement

--- urc-entrance.php
+++ urc-entrance.php
@@ -121,27 +121,6 @@ function urc_mb_logged_out() {
 
 function urc_actual_gooads() {
 
-    return '<div>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0947746501358966"
-                     crossorigin="anonymous"></script>
-                <!-- Page & Post Article Body Resposive Ad -->
-                <ins class="adsbygoogle"
-                     style="display:block"
-                     data-ad-client="ca-pub-0947746501358966"
-                     data-ad-slot="7597430493"
-                     data-ad-format="auto"
-                     data-full-width-responsive="true"></ins>
-                <script>
-                     (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-            </div>';
-
-}
-
-
-add_action( 'genesis_before_content', 'urc_display_gooads', $priority );
-function urc_display_gooads() {
-
     global $post;
 
     $hide_in_pages = array(
@@ -150,9 +129,21 @@ function urc_display_gooads() {
 
     if( is_object( $post ) && !in_array( $post->post_name, $hide_in_pages ) ) :
 
-        echo '<div>'.urc_actual_gooads().'</div>';
+        return '<div>
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0947746501358966"
+                         crossorigin="anonymous"></script>
+                    <!-- Page & Post Article Body Resposive Ad -->
+                    <ins class="adsbygoogle"
+                         style="display:block"
+                         data-ad-client="ca-pub-0947746501358966"
+                         data-ad-slot="7597430493"
+                         data-ad-format="auto"
+                         data-full-width-responsive="true"></ins>
+                    <script>
+                         (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>';
 
     endif;
-
+    
 }
-
